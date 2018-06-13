a2a43682e723b6a920fd83cf4c6ca69109d22ff5
Jonathan Goodwin <jondgoodwin@gmail.com>
2018-06-13 17:59:47 +1000

    Add Internet server socket support

--- src/main.cpp
+++ src/main.cpp
@@ -6,27 +6,40 @@
 #include "BitFunnel/Configuration/IFileSystem.h"
 #include "QueryRunner.h"
 
+#ifndef _WIN32
+int serverInit();
+int serverPoll();
+#endif
+
 int main(int argc, char *argv[]) {
-	if (argc > 1 && **argv != '\0') {
-		;
-	}
-	
+	bool server = (argc > 1 && strcmp(argv[1],"server") == 0);
+
+	// Initialize QueryRunner
 	auto fileSystem = BitFunnel::Factories::CreateFileSystem();
 	auto runner = new QueryRunner(*fileSystem,
 		                          std::cin, std::cout, 
 		                          "config",
-		                          "sonnets",
-								  8,
+		                          "manifest.txt",
+								  1,
 		                          2000000000);
 
-	while (1) {
-		std::cout << "Query (or quit): ";
-		std::string query;
-		std::getline(std::cin, query);
-		if (strcmp(query.c_str(), "quit") == 0)
-			break;
-		runner->RunQuery2(query);
+	// Handle queries via console input/output 
+	if (!server) {
+		while (1) {
+			std::cout << "Query (or quit): ";
+			std::string query;
+			std::getline(std::cin, query);
+			if (strcmp(query.c_str(), "quit") == 0)
+				break;
+			runner->RunQuery2(query);
+		}
 	}
 
-	exit(0);
+#ifndef _WIN32	
+	// Handle queries via Internet sockets
+	else {
+		serverInit();
+		serverPoll();
+	}
+#endif
 }
